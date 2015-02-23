f995142ef7bea6018fd1e668ccd363baf28fcf27
Peter Vliet <petervliet@gmail.com>
2015-02-23 11:51:25 -0700

    users can CRUD courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'users#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  resources :users
+  resources :users, :courses
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
