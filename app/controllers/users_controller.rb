2ca91f9a67d31dafc9c0367515a4f53e916c07eb
Peter Vliet <petervliet@gmail.com>
2015-02-23 10:47:51 -0700

    user can CRUD users with corresponding routes and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,51 @@
 class UsersController < ApplicationController
 
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+
+    @user.destroy
+    redirect_to root_path
   end
 
+  private
   def user_params
+    params.require(:user).permit(:user_name, :password, :about)
   end
 
 end
